60f07e090ebd2270df5ee4302b9b78c10274e097
ksolis20 <90932191+ksolis20@users.noreply.github.com>
2021-11-24 10:43:24 -0600

    Update exec.py

--- exec.py
+++ exec.py
@@ -4,7 +4,7 @@ import re
 reversed = str(sys.argv[1:])
 
 def ugh():
-    string1 = " ".join(re.findall("[a-zA-Z]+", reversed))
+    string1 = " ".join(re.findall("[a-zA-Z!.]+", reversed))
     rev = string1[::-1]
     print(rev.swapcase())
 
